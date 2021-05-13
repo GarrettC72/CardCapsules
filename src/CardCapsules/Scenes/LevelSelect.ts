8c0592df6ce8915f22f7b3b1a932e0a5e394659d
Sheng Wei Zhu <shengwei.zhu@stonybrook.edu>
2021-05-13 11:51:49 -0400

    added level select map, changed colors of buttons in level select

--- src/CardCapsules/Scenes/LevelSelect.ts
+++ src/CardCapsules/Scenes/LevelSelect.ts
@@ -21,16 +21,19 @@ export default class LevelSelect extends Scene {
 
     animatedSprite: AnimatedSprite;
     private levelSelect: Layer;
+    private lockedColor: Color = new Color(80, 80, 80);
 
     loadScene(): void {
         // Load the menu song
         this.load.audio("menu", "card-capsules_assets/Music/TitleScreen.mp3");
+        this.load.image("levelSelectMap", "card-capsules_assets/sprites/levelSelectMap.png");
     }
 
     startScene(): void {
         //Add a background layer and set the background image on it
         this.addParallaxLayer("bg", new Vec2(0, 0), -100);
-        let bg = this.add.sprite("splash_background", "bg");
+        let bg = this.add.sprite("levelSelectMap", "bg");
+        bg.scale = new Vec2(4, 4);
         bg.position.set(bg.boundary.halfSize.x, bg.boundary.halfSize.y);
 
         // Center the viewport
@@ -48,7 +51,7 @@ export default class LevelSelect extends Scene {
         levelHeader.setTextColor(Color.BLACK);
         levelHeader.fontSize = 60;
 
-        const level1Btn = <Button>this.add.uiElement(UIElementType.BUTTON, "LevelSelect", {position: new Vec2(size.x - 496, size.y + 64), text: "1"});
+        const level1Btn = <Button>this.add.uiElement(UIElementType.BUTTON, "LevelSelect", {position: new Vec2(size.x - 496 + 44, size.y + 64 + 44), text: "1"});
         level1Btn.size.set(88, 88);
         level1Btn.borderWidth = 2;
         level1Btn.borderColor = Color.BLACK;
@@ -56,47 +59,47 @@ export default class LevelSelect extends Scene {
         level1Btn.setBackgroundColor(buttonColor);
         level1Btn.onClickEventId = "level1";
 
-        const level2Btn = <Button>this.add.uiElement(UIElementType.BUTTON, "LevelSelect", {position: new Vec2(size.x - 440, size.y + 220), text: "2"});
+        const level2Btn = <Button>this.add.uiElement(GameLevel.unlockedLevel >= 2 ? UIElementType.BUTTON : UIElementType.LABEL, "LevelSelect", {position: new Vec2(size.x - 440 + 44, size.y + 220 + 44), text: "2"});
         level2Btn.size.set(88, 88);
         level2Btn.borderWidth = 2;
         level2Btn.borderColor = Color.BLACK;
         level2Btn.setTextColor(Color.WHITE);
-        level2Btn.setBackgroundColor(GameLevel.unlockedLevel >= 2 ? buttonColor : Color.RED);
+        level2Btn.setBackgroundColor(GameLevel.unlockedLevel >= 2 ? buttonColor : this.lockedColor);
         level2Btn.onClickEventId = "level2";
 
-        const level3Btn = <Button>this.add.uiElement(UIElementType.BUTTON, "LevelSelect", {position: new Vec2(size.x + 148, size.y + 196), text: "3"});
+        const level3Btn = <Button>this.add.uiElement(GameLevel.unlockedLevel >= 3 ? UIElementType.BUTTON : UIElementType.LABEL, "LevelSelect", {position: new Vec2(size.x + 148 + 44, size.y + 196 + 44), text: "3"});
         level3Btn.size.set(88, 88);
         level3Btn.borderWidth = 2;
         level3Btn.borderColor = Color.BLACK;
         level3Btn.setTextColor(Color.WHITE);
-        level3Btn.setBackgroundColor(GameLevel.unlockedLevel >= 3 ? buttonColor : Color.RED);
+        level3Btn.setBackgroundColor(GameLevel.unlockedLevel >= 3 ? buttonColor : this.lockedColor);
         level3Btn.onClickEventId = "level3";
 
-        const level4Btn = <Button>this.add.uiElement(UIElementType.BUTTON, "LevelSelect", {position: new Vec2(size.x + 368, size.y - 8), text: "4"});
+        const level4Btn = <Button>this.add.uiElement(GameLevel.unlockedLevel >= 4 ? UIElementType.BUTTON : UIElementType.LABEL, "LevelSelect", {position: new Vec2(size.x + 368 + 44, size.y - 8 + 44), text: "4"});
         level4Btn.size.set(88, 88);
         level4Btn.borderWidth = 2;
         level4Btn.borderColor = Color.BLACK;
         level4Btn.setTextColor(Color.WHITE);
-        level4Btn.setBackgroundColor(GameLevel.unlockedLevel >= 4 ? buttonColor : Color.RED);
+        level4Btn.setBackgroundColor(GameLevel.unlockedLevel >= 4 ? buttonColor : this.lockedColor);
         level4Btn.onClickEventId = "level4";
 
-        const level5Btn = <Button>this.add.uiElement(UIElementType.BUTTON, "LevelSelect", {position: new Vec2(size.x + 332, size.y - 308), text: "5"});
+        const level5Btn = <Button>this.add.uiElement(GameLevel.unlockedLevel >= 5 ? UIElementType.BUTTON : UIElementType.LABEL, "LevelSelect", {position: new Vec2(size.x + 332 + 44, size.y - 308 + 44), text: "5"});
         level5Btn.size.set(88, 88);
         level5Btn.borderWidth = 2;
         level5Btn.borderColor = Color.BLACK;
         level5Btn.setTextColor(Color.WHITE);
-        level5Btn.setBackgroundColor(GameLevel.unlockedLevel >= 5 ? buttonColor : Color.RED);
+        level5Btn.setBackgroundColor(GameLevel.unlockedLevel >= 5 ? buttonColor : this.lockedColor);
         level5Btn.onClickEventId = "level5";
 
-        const level6Btn = <Button>this.add.uiElement(UIElementType.BUTTON, "LevelSelect", {position: new Vec2(size.x - 232, size.y - 328), text: "6"});
+        const level6Btn = <Button>this.add.uiElement(GameLevel.unlockedLevel >= 6 ? UIElementType.BUTTON : UIElementType.LABEL, "LevelSelect", {position: new Vec2(size.x - 232 + 44, size.y - 328 + 44), text: "6"});
         level6Btn.size.set(88, 88);
         level6Btn.borderWidth = 2;
         level6Btn.borderColor = Color.BLACK;
         level6Btn.setTextColor(Color.WHITE);
-        level6Btn.setBackgroundColor(GameLevel.unlockedLevel >= 6 ? buttonColor : Color.RED);
+        level6Btn.setBackgroundColor(GameLevel.unlockedLevel >= 6 ? buttonColor : this.lockedColor);
         level6Btn.onClickEventId = "level6";
 
-        const levelBack = <Button>this.add.uiElement(UIElementType.BUTTON, "LevelSelect", {position: new Vec2(size.x, size.y + 350), text: "Back"});
+        const levelBack = <Button>this.add.uiElement(UIElementType.BUTTON, "LevelSelect", {position: new Vec2(size.x - 450, size.y - 350), text: "Main Menu"});
         levelBack.size.set(200, 50);
         levelBack.borderWidth = 2;
         levelBack.borderColor = Color.BLACK;
